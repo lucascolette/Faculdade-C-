f62698a1c2f94a70b288e97735cec44c74c79bb0
Lucas Colette <eu@colet.me>
2012-02-28 00:36:07 -0300

    Indentação fixada

--- AnoNascimento/AnoNascimento/Main.cs
+++ AnoNascimento/AnoNascimento/Main.cs
@@ -5,27 +5,29 @@ using System.Text;
 
 namespace AnoNascimento
 {
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			/**
-			 * @author Lucas Colette <eu@colet.me>
-			 * @RA 212114030
-			 * 
-			 * Um programa que solicite a idade do usuario e informe o ano de seu nascimento
-			 *
-			 */
-			
+  class MainClass
+  {
+    public static void Main (string[] args)
+    {
+		
+		/**
+		 * @author Lucas Colette <eu@colet.me>
+		 * @RA 212114030
+		 * 
+		 * Um programa que solicite a idade do usuario e 
+		 * informe o ano de seu nascimento
+		 *
+		 */
+
 			Console.WriteLine("Qual a sua idade ?");
 
 			int Idade = Convert.ToInt32(Console.ReadLine());
-			
+
 			DateTime Hoje = DateTime.Now;
 			int AnoAtual = Hoje.Year;
 
 			Console.WriteLine("Você nasceu no ano de " + (AnoAtual-Idade));
-			
-		}
+
+		}	
 	}
 }
